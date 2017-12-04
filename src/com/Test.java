e42a62769a1c33fc9d8b65152fade1ab5c9f99de
cwt <1204737286@qq.com>
2017-12-04 12:31:32 +0800

    点点滴滴

--- src/com/Test.java
+++ src/com/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 		
-		System.out.println("ssfdfdddddsf点点滴滴");
+		System.out.println("ssfdfdd硕士生dddsf点点滴滴");
 	}
 
 }
